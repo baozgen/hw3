9ccfdf83b66de0e117f2322b4f2afbe0e60bb8d3
baozgen <baris.ozgen@kellogg.northwestern.edu>
2023-02-21 14:29:45 +0000

    adds code

---
+++ app/controllers/places_controller.rb
@@ -0,0 +1,42 @@
+class PlacesController < ApplicationController
+    
+    def index
+        @places = Place.all
+      end
+    
+      def show
+        @place = Place.find_by({ "id" => params["id"] })
+      end
+    
+      def new
+        @place = Place.new
+      end
+    
+      def create
+        @place = Place.new
+        @place["name"] = params["place"]["name"]
+        @place.save
+        redirect_to "/places"
+      end
+    
+=begin
+      def edit
+        @place = Place.find_by({ "id" => params["id"] })
+      end
+    
+      def update
+        @place = Place.find_by({ "id" => params["id"] })
+        @place["name"] = params["place"]["name"]
+        @place.save
+        redirect_to "/places"
+      end
+    
+      def destroy
+        @place = Place.find_by({ "id" => params["id"] })
+        @place.destroy
+        redirect_to "/places"
+      end
+
+=end 
+
+end
